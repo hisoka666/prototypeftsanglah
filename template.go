7b0eb10a7ea445fa82beb4a01f0bd3573cbc464b
deathscythehell <pale.deathscythe@gmail.com>
2016-09-15 17:55:20 +0800

    edit app.yaml dan template.go

--- template.go
+++ template.go
@@ -1,58 +1,75 @@
 package main
 
 import (
-   "log"
-   "fmt"
-   "net/http"
-   "html/template"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
-func main(){
-http.HandleFunc("/", index)
-http.HandleFunc("/main", mainPage)
-http.HandleFunc("/login", login)
-http.HandleFunc("/register", register)
-http.HandleFunc("/searchCM", searchCM)
-http.HandleFunc("/tambahdata", tambahdata)
-log.Print("Listening on port 8080")
-log.Fatal(http.ListenAndServe(":8080", nil))
+func init() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/main", mainPage)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/searchCM", searchCM)
+	http.HandleFunc("/tambahdata", tambahdata)
+	http.HandleFunc("/welcome", welcome)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
-func index(w http.ResponseWriter, r *http.Request){
-  if r.URL.Path != "/"{
-    http.NotFound(w, r)
-    return
-  }
-  s1, _ := template.ParseFiles("templates/base.html", "templates/index.html")
-  s1.Execute(w, nil)
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	if u == nil {
+		url, _ := user.LoginURL(ctx, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		return
+	}
+	url, _ := user.LogoutURL(ctx, "/")
+	fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
 }
 
-func searchCM(w http.ResponseWriter, r *http.Request){
-  nocm := r.PostFormValue("nocm")
-  fmt.Fprint(w, nocm)
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	s1, _ := template.ParseFiles("templates/base.html", "templates/index.html")
+	s1.Execute(w, nil)
 }
+
+func searchCM(w http.ResponseWriter, r *http.Request) {
+	nocm := r.PostFormValue("nocm")
+	fmt.Fprint(w, nocm)
+}
+
 //TODO:fungsi login
-func login(w http.ResponseWriter, r *http.Request){
-
+func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
 //TODO:fungsi tambah data pasien
 
-func tambahdata(w http.ResponseWriter, r *http.Request){
-
+func tambahdata(w http.ResponseWriter, r *http.Request) {
 
 }
+
 //TODO:fungsi registrasi
-func register(w http.ResponseWriter, r *http.Request){
+func register(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func mainPage(w http.ResponseWriter, r *http.Request){
-  if r.URL.Path != "/main"{
-    http.NotFound(w, r)
-    return
-  }
-  s1, _ := template.ParseFiles("templates/base.html", "templates/main.html")
-  s1.Execute(w, nil)
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/main" {
+		http.NotFound(w, r)
+		return
+	}
+	s1, _ := template.ParseFiles("templates/base.html", "templates/main.html")
+	s1.Execute(w, nil)
 }
